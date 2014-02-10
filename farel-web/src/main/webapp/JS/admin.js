7a4cd59d8022b37aec56590d7c4396560ec68e34
Mikołaj Nałęcz <mikolaj.nalecz@hotmail.com>
2014-02-10 13:24:57 +0100

    Added editing option to admin panel.
    Change-Id: Ibb70d98449fa647820052e9ef245d9dfb3cde54f

--- farel-web/src/main/webapp/JS/admin.js
+++ farel-web/src/main/webapp/JS/admin.js
@@ -1,4 +1,13 @@
 function Controller($scope, $http) {
+
+    function getUrlVars() {
+        var vars = {};
+        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+            vars[key] = value;
+        });
+        return vars;
+    }
+
     $http({
                     method : 'GET',
                     url : 'rest/projects'
@@ -28,17 +37,22 @@ function Controller($scope, $http) {
     };
 
     $scope.editProject = function(project) {
-        //$rootScope.chosenProject = project;
-        //Nawet zadeklarowany w rootScopie nie jest widoczny z edit.html przez co nie dziala PUT
-        window.location.replace("http://localhost:8080/edit.html");
+        // $rootScope.chosenProject = project;
+        // Nawet zadeklarowany w rootScopie nie jest widoczny z edit.html przez
+        // co nie dziala PUT
+        window.location.replace("http://localhost:8080/edit.html?id=" + project.id + "&name=" + project.name);
     };
 
     $scope.saveProject = function(project) {
+        project.id = getUrlVars()["id"];
         $http({
-                        method : 'PUT',
-                        url : 'rest/projects/' + project.name,
+                        method : 'POST',
+                        url : 'rest/projects',
                         data : project
+        }).success(function() {
+            window.location.replace("http://localhost:8080/admin.html");
+        }).error(function() {
+            alert("Project with this name already exists!");
         });
-        window.location.replace("http://localhost:8080/admin.html");
     };
 }
